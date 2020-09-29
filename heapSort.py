505e477864e08ca68e54955327331c649625e665
Zijun Xu <zixu6769@colorado.edu>
2020-09-29 14:32:53 -0400

    Add files via upload

--- heapSort.py
+++ heapSort.py
@@ -1,20 +1,45 @@
 import math
 
-class Heap:
-	def __init__(self, array):
-		self.array = array
+def parent(i):
+	return math.floor((i-1)/2)
 
-	def parent(self, i):
-		return self.array[math.floor((i-1)/2)]
+def left(i):
+	return 2*i + 1
 
-	def left(self, i):
-		return self.array[2*i + 1]
+def right(i):
+	return 2*i + 2
 
-	def right(self, i):
-		return self.array[2*i + 2]
+def maxHeapify(A, i, size):
+	l = left(i)
+	r = right(i)
+	if (l <= size-1 and A[l] > A[i]):
+		largest = l
+	else:
+		largest = i
+	if (r <= size-1 and A[r] > A[largest]):
+		largest = r
+	if (largest != i):
+		temp = A[i]
+		A[i] = A[largest]
+		A[largest] = temp
+		maxHeapify(A, largest, size)
 
-def maxHeapify()
+def buildMaxHeap(A):
+	size = len(A)
+	for i in range(math.floor(len(A)/2)-1, -1, -1):
+		maxHeapify(A, i, size)
+	
+def heapSort(A):
+	size = len(A)
+	buildMaxHeap(A)
+	for i in range(len(A)-1, 0, -1):
+		temp = A[0]
+		A[0] = A[i]
+		A[i] = temp
+		size -= 1
+		print(A)
+		maxHeapify(A, 0, size)
 
-
-def heapSort(array):
-	h = Heap(array)
+if __name__ == '__main__':
+	array = [0,1,2,3,4,5,6]
+	heapSort(array)
