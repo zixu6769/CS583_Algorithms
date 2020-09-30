96fe155eca0c67b52dd014de01bc04ba370bf189
Zijun Xu <zixu6769@colorado.edu>
2020-09-30 17:31:12 -0400

    Add files via upload

--- p6-1.py
+++ p6-1.py
@@ -1,5 +1,10 @@
 from heapSort import *
 
+'''
+We can build a heap by repeatedly calling MAX-HEAP-INSERT to insert the elements
+into the heap.
+'''
+
 def buildMaxHeapPrime(A):
 	a = []
 	h = Heap(a)
@@ -12,6 +17,7 @@ if __name__ == '__main__':
 	print("*** Problem 6-1 ***")
 	A = [4,1,3,2,16,9,10,14,8,7]
 	print("original array     ", A)
+	print("they do not produce the same heap!")
 	print("buildMaxHeap:      ", buildMaxHeap(Heap(A)))
 	print("buildMaxHeapPrime: ", buildMaxHeapPrime(Heap(A)))
 	
